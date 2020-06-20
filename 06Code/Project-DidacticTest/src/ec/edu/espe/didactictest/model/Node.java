a7fe282efb8f63f98fa2351d29bd74db17be6c72
bryanyaguarshungo <bryanyaguarshungo@gmail.com>
2020-06-20 11:42:13 -0500

    commitnode

--- 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/model/Node.java
+++ 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/model/Node.java
@@ -10,5 +10,34 @@ package ec.edu.espe.didactictest.model;
  * @author USER
  */
 public class Node {
+    private T elemento;
+    private Node<T> siguiente; 
+
+ 
+    public Node(T elemento, Node<T> siguiente) {
+        this.elemento = elemento;
+        this.siguiente = siguiente;
+    }
+
+    public T getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+
+    public Node<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Node<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return elemento + "\n";
+    }
     
 }
