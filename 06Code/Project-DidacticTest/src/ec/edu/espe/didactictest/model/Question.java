86cdddc01b07490297af7e8dbdf7c8aa7f8ee86a
roberthy31 <57681362+roberthy31@users.noreply.github.com>
2020-06-20 10:43:30 -0500

    QuestionClass

--- 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/model/Question.java
+++ 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/model/Question.java
@@ -10,5 +10,48 @@ package ec.edu.espe.didactictest.model;
  * @author USER
  */
 public class Question {
+    private String question;
+    private ListQuestion<Answer> answers;
+    private int points;
     
+    public Question(String question, ListQuestion<Answer> answers, int points) {
+        this.question = question;
+        this.answers = answers;
+        this.points = points;
+    }
+    
+    public String getQuestion() {
+        return question;
+    }
+
+    public ListQuestion<Answer> getAnswers() {
+        return answers;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+    
+    
+    public void showQuestion(){
+        System.out.println(question);
+        
+        int i=0;
+        
+        while(i<answers.size()){
+            
+            System.out.println("\t  " + (i+1) + answers.get(i));
+            i++;
+        }
+    }
+        
+    public boolean checkAnswer(int answer){
+        Answer nr1 = answers.get(answer - 1);
+        
+        if (nr1 != null)
+        {
+            return nr1.isCorrectly();
+        }
+        return false;
+    }
 }
