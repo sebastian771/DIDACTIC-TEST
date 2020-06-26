08828f7a242569c054f3afa4d9a2f2a98e501154
roberthy31 <57681362+roberthy31@users.noreply.github.com>
2020-06-25 23:33:43 -0500

    RefactoreAndDelet

--- 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/utils/FileManager.java
+++ 06Code/Project-DidacticTest/src/ec/edu/espe/didactictest/utils/FileManager.java
@@ -18,7 +18,6 @@ import java.util.logging.Logger;
 public class FileManager {
 
     public boolean removeFile(String fileName) {
-
         try {
             File fichero = new File(fileName);
             if (fichero.delete()) {
@@ -26,7 +25,6 @@ public class FileManager {
             } else {
                 System.out.println("Error");
             }
-
         } catch (Exception e) {
             System.err.println("Error---->" + e.getMessage());
         }
@@ -35,25 +33,20 @@ public class FileManager {
 
     public boolean openFileForRead(String fileName) {
         try {
-
             File objetofile = new File(fileName);
             Desktop.getDesktop().open(objetofile);
-
         } catch (IOException ex) {
             System.out.println(ex);
         }
-
         return true;
     }
 
     public boolean createFile(String fileName) {
         File archivetxt;
-
+        
         try {
             System.out.println("creating File" + fileName);
-
             archivetxt = new File(fileName);
-
             if (!archivetxt.exists()) {
                 try {
                     archivetxt.createNewFile();
@@ -63,12 +56,9 @@ public class FileManager {
             } else {
                 System.out.println("Archive ---->" + fileName + "<---- Alredy exist");
             }
-
             return true;
-
         } catch (Exception ex) {
             System.out.println("Messages and acctions taken after a fail");
-
             return false;
         }
     }
@@ -77,7 +67,7 @@ public class FileManager {
         File archivetxt;
         FileWriter write;
         PrintWriter line;
-
+        
         try {
             System.out.println("saving" + result + "into " + fileName);
 
@@ -94,7 +84,6 @@ public class FileManager {
                 } catch (IOException ex) {
                     Logger.getLogger(DidacticTest.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
             } else {
                 try {
                     write = new FileWriter(archivetxt, true);
@@ -107,7 +96,6 @@ public class FileManager {
                     Logger.getLogger(DidacticTest.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-
             return true;
         } catch (Exception ex) {
             System.out.println("Writing a string infile a fail");
@@ -120,17 +108,17 @@ public class FileManager {
             String data = "";
             System.out.println("reading from File " + fileName);
             String cadena;
+            
             FileReader f = new FileReader(fileName);
             BufferedReader b = new BufferedReader(f);
+            
             while ((cadena = b.readLine()) != null) {
                 System.out.println(cadena);
-            }
-            b.close();           
+            }            
+            b.close();
             return data;
-
         } catch (Exception ex) {
             System.out.println("Reading file " + fileName + " Failed :( ");
-
             return "";
         }
     }
